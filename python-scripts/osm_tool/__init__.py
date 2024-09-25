6df3e566c12cad5eb3d47b1c9a8d6b8c96199083
Matheus D C Roque <matheusdcroque@gmail.com>
2024-09-25 17:38:47 -0300

    Last changes in python

--- python-scripts/osm_tool/__init__.py
+++ python-scripts/osm_tool/__init__.py
@@ -6,6 +6,14 @@ import json
 import cli
 
 default_needed_keys = ["osmid", "oneway", "lanes", "reversed", "maxspeed", "highway"]
+result_dict_needed = {
+    "osmid": str,
+    "oneway": bool,
+    "lanes": int,
+    "reversed": bool,
+    "maxspeed": float,
+    "highway": str,
+}
 
 #para vias urbanas:
 # – trânsito rápido: 80 km/h;
@@ -18,10 +26,41 @@ default_speeds = {
     "residential": 30,
 }
 
-def process_args():
-    for _, arg in vars(cli.args).items():
-        argtype = type(arg)
-        print(f"{arg}: {argtype}")
+# CLI interaction functions
+
+def process_args() -> dict:
+    request_info = {}
+    if cli.args.place is not None:
+        request_info["retrieval_type"] = "place"
+        request_info["place"] = cli.args.place
+    elif cli.arg.latitude is not None and cli.arg.longitude is not None and cli.arg.radius is not None:
+        request_info["retrieval_type"] = "coordinates"
+        request_info["latitude"] = cli.args.latitude
+        request_info["longitude"] = cli.args.longitude
+        request_info["radius"] = cli.args.radius
+    else:
+        request_info["retrieval_type"] = "noop"
+        return request_info
+    
+    request_info["path"] = cli.args.file_path if cli.args.file_path is not None else "../output"
+    request_info["name"] = cli.args.name if cli.args.name is not None else "default"
+    request_info["save_results"] = not cli.args.dry_run
+
+    return request_info
+    
+def process_request(request_info: dict) -> bool:
+    nw = None
+    match request_info["retrieval_type"]:
+        case "place":
+            nw = get_graph_from_place(request_info["place"])
+        case "coordinates":
+            nw = get_graph_from_coord(request_info["latitude"], request_info["longitude"], request_info["radius"])
+        case _:
+            print("Noop")
+            return False
+    return save_graph(graph=nw, graph_name=request_info["name"], base_path=request_info["path"], prune_keys=["geometry"], inverted_prune=False)
+
+# OSMNX graph retrieval functions
 
 def get_graph_from_place(place: str) -> nx.MultiDiGraph:
     nw = ox.graph_from_place(query=place, simplify=True, network_type='drive')
@@ -36,13 +75,7 @@ def get_graph_from_coord(lati: float, long: float, radius: int) -> nx.MultiDiGra
     nw = ox.add_edge_travel_times(nw)
     return nw
 
-def create_path(path: str) -> bool:
-    try:
-        pathlib.Path(path).mkdir(parents=True, exist_ok=True)
-        return True
-    except Exception as e:
-        print(f"Failed to create path {path} with exception: {e}")
-        return False
+# Graph processing functions
 
 def save_graph(
         graph: nx.MultiDiGraph, 
@@ -50,7 +83,7 @@ def save_graph(
         base_path: str = "output", 
         prune_keys: list[str] = [],
         inverted_prune: bool = False,
-        needed_keys: list[str] = default_needed_keys) -> bool:
+        needed_keys: list[str] = result_dict_needed) -> bool:
     path = base_path + "/" + graph_name + "/"
 
     if not create_path(path):
@@ -63,7 +96,7 @@ def save_graph(
 
     if len(prune_keys) >= 1:
         graph = prune_graph_info(graph, prune_keys, inverted_prune)
-
+            
     if not save_nodes_info(graph, path):
         print(f"Error saving graph at {path}: Failed to save node data")
         return False
@@ -73,6 +106,14 @@ def save_graph(
         return False
 
     return True
+
+def create_path(path: str) -> bool:
+    try:
+        pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+        return True
+    except Exception as e:
+        print(f"Failed to create path {path} with exception: {e}")
+        return False
 
 def check_graph_integrity(graph: nx.MultiDiGraph, needed_keys: list[str]) -> bool:
     def try_fix(missing_field: str, edge_info: dict) -> any:
@@ -99,7 +140,7 @@ def check_graph_integrity(graph: nx.MultiDiGraph, needed_keys: list[str]) -> boo
         if not edge[0] in node_list or not edge[1] in node_list:
             print(f"Edge ({edge[0]}, {edge[1]}) not found in node data")
             return False
-        for key in needed_keys:
+        for key, value_type in needed_keys.items():
             if key not in edge[2]:
                 # print(f"WARN Edge ({edge[0]}, {edge[1]}) missing '{key}' data, trying default values")
                 try:
@@ -109,6 +150,22 @@ def check_graph_integrity(graph: nx.MultiDiGraph, needed_keys: list[str]) -> boo
                     print("Failed to fix. Full edge info: \n")
                     print(edge[2])
                     return False
+            if type(edge[2][key]) == list:
+                try:
+                    edge[2][key] = [value_type(x) for x in edge[2][key]]
+                    edge[2][key] = min(edge[2][key])
+                except:
+                    print(f"Type fixing error: List {edge[2][key]} from [{key}] for type {value_type} has no minimum")
+                    print(edge)
+                    return False
+            elif type(edge[2][key]) != value_type:
+                try:
+                    edge[2][key] = value_type(edge[2][key])
+                except:
+                    print(f"Type fixing error: {edge[2][key]} from [{key}] for type {value_type}")
+                    print(edge)
+                    return False
+
     return True
 
 def prune_graph_info(graph: nx.MultiDiGraph, prune_keys: list[str], is_inverted: bool = False) -> nx.MultiDiGraph:
@@ -132,7 +189,7 @@ def save_nodes_info(graph: nx.MultiDiGraph, path: str) -> bool:
         data = list(graph.nodes(data=True))
         fullpath = path + "/nodes.json"
         with open(fullpath, 'w') as f:
-            json.dump(data, f, indent=4)
+            json.dump(data, f, indent=4, ensure_ascii=False)
         return True
     except Exception as e:
         print(f"Exception: {e}")
@@ -143,21 +200,20 @@ def save_edges_info(graph: nx.MultiDiGraph, path: str) -> bool:
         data = list(graph.edges(data=True))
         fullpath = path + "/edges.json"
         with open(fullpath, 'w') as f:
-            json.dump(data, f, indent=4)
+            json.dump(data, f, indent=4, ensure_ascii=False)
         return True
     except Exception as e:
         print(f"Exception: {e}")
         return False
 
-def main():
-    process_args()
-    place = "Jose Mendes, Florianópolis"
-    nw = get_graph_from_place(place)
+# Main function
 
-    for edge in list(nw.edges(data=True)):
-        if not "maxspeed" in edge[2] and not "speed_kph" in edge[2]:
-            print(edge)
-    save_graph(graph=nw, graph_name="grafinho2", base_path="../teste", prune_keys=["geometry"], inverted_prune=False)
+def main():
+    request = process_args()
+    if process_request(request_info=request):
+        print("Success")
+    else:
+        print("Failed")
 
 if __name__ == "__main__":
     main()
--- python-scripts/osm_tool/cli.py
+++ python-scripts/osm_tool/cli.py
@@ -1,9 +1,12 @@
 import argparse
 
+# Example
+# poetry run python3 osm_tool/__init__.py -p "José Mendes, Florianópolis" -n jose_mendes
+
 parser = argparse.ArgumentParser(description='Retrieve OSM graphs and data')
-parser.add_argument('-s',
-                    '--save',
-                    type=str,
+parser.add_argument('-d',
+                    '--dry_run',
+                    action='store_true',
                     help='Save results to file')
 
 parser.add_argument('-p',
@@ -25,5 +28,15 @@ parser.add_argument('-r',
                     '--radius',
                     type=int,
                     help='Radius around the center of data retrieval')
+
+parser.add_argument('-f',
+                    '--file_path',
+                    type=str,
+                    help='Path for storing results')
+
+parser.add_argument('-n',
+                    '--name',
+                    type=str,
+                    help='Name for the data retrieval results')
 
 args = parser.parse_args()
